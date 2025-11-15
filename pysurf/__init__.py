e838d608b82612827765dd22f5f66002e183ba5f
Joel Ibaceta <mail@joelibaceta.com>
2025-11-14 23:04:45 -0500

    make installable

--- pysurf/__init__.py
+++ pysurf/__init__.py
@@ -2,6 +2,8 @@
 pySURF - SURF (Speeded-Up Robust Features) implementation in Python.
 """
 
+__version__ = "0.1.0"
+
 from pysurf.surf import Surf
 
-__all__ = ['Surf']
+__all__ = ['Surf', '__version__']
